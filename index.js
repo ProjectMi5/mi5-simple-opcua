a98718d8ad7d73bbfd05e72ab207c2492157f016
Dominik Serve <serve@itq.de>
2016-06-14 06:09:04 +0200

    folderStructureHelper renamed and added

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 module.exports.OpcuaServer = require('./models/opcua-server');
 module.exports.OpcuaClient = require('./models/opcua-client');
 module.exports.OpcuaVariable = require('./models/opcua-variable');
+module.exports.helper = require('./helper/folderStructureHelper');
